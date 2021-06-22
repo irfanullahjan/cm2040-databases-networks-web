f4a12ca7deca8fb2c2f4b78abb1a900325605e2e
Irfanullah Jan <irfanullah.jan@outlook.com>
2021-06-22 10:58:45 +0000

    Added Express and EJS

--- topic7/mid-term/index.js
+++ topic7/mid-term/index.js
@@ -1,12 +1,8 @@
-var http = require("http");
-
-http
-  .createServer(function(req, res) {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.write("Welcome to the mid-term application! \n\n");
-    res.write("This application must run on PORT 8089");
-    res.end();
-  })
-  .listen(8089, function() {
-    console.log("Node server is running...");
-  });
+const express = require("express");
+const app = express();
+const port = 8089;
+require("./routes/main")(app);
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
