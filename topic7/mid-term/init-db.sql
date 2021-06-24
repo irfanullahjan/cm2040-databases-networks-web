1e6bba000a4455163b9280eae4656e63cdc4fe05
irfanullahjan <irfanullah.jan@outlook.com>
2021-06-24 17:11:25 +0500

    Table relations nearly complete

--- topic7/mid-term/init-db.sql
+++ topic7/mid-term/init-db.sql
@@ -2,12 +2,43 @@
 -- Run these to populate initial data IN the database
 -- Assumes that Database named `smarthome` is already created USE smarthome;
 
-CREATE TABLE devicetypes(
+CREATE TABLE device_types(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(64) NOT NULL
 );
 
-INSERT INTO devicetypes
+CREATE TABLE config_types(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(64) NOT NULL,
+  input VARCHAR(16) NOT NULL,
+  presets JSON
+);
+
+CREATE TABLE device_types_configs(
+  device_type INT NOT NULL,
+  config_type INT NOT NULL,
+  PRIMARY KEY (device_type, config_type),
+  FOREIGN KEY (device_type) REFERENCES device_types(id) ON DELETE CASCADE,
+  FOREIGN KEY (config_type) REFERENCES config_types(id) ON DELETE CASCADE
+);
+
+CREATE TABLE user_devices(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  device_type INT NOT NULL,
+  name VARCHAR(64) NOT NULL,
+  FOREIGN KEY (device_type) REFERENCES device_types(id)
+);
+
+CREATE TABLE user_devices_configs(
+  user_device INT NOT NULL,
+  config_type INT NOT NULL,
+  PRIMARY KEY (user_device, config_type),
+  value VARCHAR(64) NOT NULL,
+  FOREIGN KEY (user_device) REFERENCES user_devices(id) ON DELETE CASCADE,
+  FOREIGN KEY (config_type) REFERENCES config_types(id) ON DELETE CASCADE
+);
+
+INSERT INTO device_types
 VALUES
   (NULL, 'Television'),
   (NULL, 'Lighting'),
@@ -18,17 +49,12 @@ VALUES
   (NULL, 'Fire Alarm'),
   (NULL, 'CCTV');
 
-CREATE TABLE configtypes( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(64) NOT NULL, input VARCHAR(16) NOT NULL, presets JSON);
+INSERT INTO config_types
+VALUES
+  (NULL, "Power", "radio", '["On", "Off"]'),
+  (NULL, "Mode", "select", '["Slow", "Medium", "Fast"]'),
+  (NULL, "Auto-sleep", "checkbox", NULL),
+  (NULL, "Volume", "number", '[0, 100]');
 
-INSERT INTO configtypes VALUES (NULL, "Power", "radio", '["On", "Off"]'), (NULL, "Mode", "select", '["Slow", "Medium", "Fast"]'), (NULL, "Auto-sleep", "checkbox", NULL), (NULL, "Volume", "number", '[0, 100]');
-
-CREATE TABLE devices( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, devicetype INT NOT NULL, name VARCHAR(64) NOT NULL, FOREIGN KEY (devicetype) REFERENCES devicetypes(id) );
-
-CREATE TABLE deviceconfigs(
-  id INT NOT NULL AUTO_INCREMENT,
-  device INT NOT NULL,
-  configtype INT NOT NULL,
-  value VARCHAR(64),
-  FOREIGN KEY (device) REFERENCES devices(id) ON DELETE CASCADE,
-  FOREIGN KEY (configtype) REFERENCES configtypes(id) ON DELETE CASCADE
-);
+INSERT INTO device_types_configs
+VALUES (1, 1), (1, 3), (1, 4), (2, 1), (3, 1), (3, 4);
--- topic7/mid-term/routes/devices/add.js
+++ topic7/mid-term/routes/devices/add.js
@@ -3,26 +3,37 @@ var router = express.Router();
 
 const view = "devices/add.html";
 
-// Add device
+// Add device form
 router.get("/:type?", function (req, res) {
-  if (!req.params["type"]) {
-    let sqlquery = "SELECT * FROM devicetypes";
-    db.query(sqlquery, (err, result) => {
+  let sqlquery = "SELECT * FROM device_types";
+  db.query(sqlquery, (err, allDevices) => {
+    if (err) {
+      res.status(500).send("Database query to fetch device types failed.");
+    }
+    sqlquery =
+      "SELECT * from device_types\
+        LEFT JOIN device_types_configs ON device_types.id = device_types_configs.device_type\
+        LEFT JOIN config_types ON config_types.id = device_types_configs.config_type\
+        WHERE device_types.id = 1;";
+    db.query(sqlquery, (err, selectedDeviceType) => {
+      console.log(selectedDeviceType);
       if (err) {
-        res.status(500).send("Database query to fetch device types failed.");
+        res
+          .status(500)
+          .send("Database query to fetch device type configs failed.");
       }
       res.render(view, {
         title: "Select a device to add",
-        selectedDevice: req.params["type"],
-        allDevices: result,
+        selectedDevice: selectedDeviceType,
+        allDevices: allDevices,
       });
     });
-  } else {
-    res.render(view, {
-      title: "Add selected device",
-      selectedDevice: req.params["type"],
-    });
-  }
+  });
+});
+
+// Add device form submitted
+router.post("/", function (req, res) {
+  res.send("Form submitted");
 });
 
 module.exports = router;
