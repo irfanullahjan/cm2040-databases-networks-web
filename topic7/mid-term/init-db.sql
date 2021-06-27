1a5ee1587963aa68ef8aea527a21764e48059742
irfanullahjan <irfanullah.jan@outlook.com>
2021-06-27 11:08:47 +0500

    more device configurations added

--- topic7/mid-term/init-db.sql
+++ topic7/mid-term/init-db.sql
@@ -50,8 +50,8 @@ VALUES
   (7, 'Fire alarm'),
   (8, 'CCTV'),
   (9, 'Water pump'),
-  (10, 'Refrigerater'),
-  (11, 'Door'),
+  (10, 'Freezer'),
+  (11, 'Water dispenser'),
   (12, 'Oven'),
   (13, 'Microwave oven'),
   (14, 'Printer'),
@@ -66,7 +66,7 @@ INSERT INTO config_types
 VALUES
   (1, "Name", "text", NULL),
   (2, "Power", "radio", '["On", "Off"]'),
-  (3, "Speed", "radio", '["Slow", "Medium", "Fast"]'),
+  (3, "RPM", "range", '[0, 1000]'),
   (4, "Auto-sleep", "checkbox", NULL),
   (5, "Auto-update", "checkbox", NULL),
   (6, "Volume", "range", '[0, 100]'),
@@ -78,31 +78,30 @@ VALUES
   (12, "Timer", "select", '["5min", "10min", "30min", "60min"]'),
   (13, "IP Address", "text", NULL),
   (14, "Tint", "select", '["Neutral", "Red", "Yellow", "Green", "Blue", "Violet"]'),
-  (15, "White balance", "select", '["Neutral", "Extra warm", "Warm", "Cool", "Extra cool"]'),
-  (16, "RPM", "range", '[0, 1000]');
+  (15, "White balance", "select", '["Neutral", "Extra warm", "Warm", "Cool", "Extra cool"]');
 
 INSERT INTO device_types_configs
 VALUES
   (1, 1), (1, 2), (1, 4), (1, 5), (1, 6), (1, 9), (1, 13), (1, 14), (1, 15), 
   (2, 1), (2, 2), (2, 5), (2, 9), (2, 14), (2, 15), 
   (3, 1), (3, 2), (3, 4), (3, 6),
-  (4, 1), (4, 2), (4, 16),
-  (5, 1),
+  (4, 1), (4, 2), (4, 3),
+  (5, 1), (5, 7), (5, 11),
   (6, 1), (6, 2), (6, 8),
-  (7, 1),
-  (8, 1),
-  (9, 1),
-  (10, 1),
-  (11, 1), (11, 7),
-  (12, 1), (12, 2), (12, 7),
-  (13, 1), (13, 2), (13, 7),
-  (14, 1),
-  (15, 1), (15, 7),
-  (16, 1),
+  (7, 1), (7, 2),
+  (8, 1), (8, 2), (8, 11), (8, 13),
+  (9, 1), (9, 2), (9, 3), (9, 12),
+  (10, 1), (10, 2), (10, 10),
+  (11, 1), (11, 2), (11, 8),
+  (12, 1), (12, 2), (12, 7), (12, 12),
+  (13, 1), (13, 2), (13, 7), (13, 8), (13, 12),
+  (14, 1), (14, 2), (14, 4),
+  (15, 1), (15, 7), (15, 14), (15, 15),
+  (16, 1), (16, 2), (16, 8),
   (17, 1), (17, 7),
   (18, 1), (18, 2), (18, 4), (18, 6), 
   (19, 1), (19, 2), (19, 13),
-  (20, 1), (20, 2), (20, 3) ;
+  (20, 1), (20, 2), (20, 3), (20, 12);
 
 
 --only for docker mysql to enable insecure authentication becuase node package 'mysql' doesn't work otherwise
