a6654151c1386a052a38f4f55e3903dde83f760f
irfanullahjan <irfanullah.jan@outlook.com>
2021-07-04 07:17:21 +0500

    delete confirmation page added

--- topic7/mid-term/routes/devices/delete.js
+++ topic7/mid-term/routes/devices/delete.js
@@ -2,22 +2,59 @@ var express = require("express");
 var router = express.Router();
 
 // Delete device confirmation page
-router.get("/:type?", function (req, res) {
-  const deviceId = parseInt(req.params["type"]) || 0;
+router.get("/:id", function (req, res) {
+  const deviceId = parseInt(req.params["id"]) || 0;
+  // if deviceId is invalid
+  if (deviceId < 1) {
+    res.status(401).send("Device id is not correct.");
+    return;
+  }
+  // check if the device being deleted actually exists in the database
+  let sqlquery = "SELECT * FROM user_devices WHERE id = ?";
+  db.query(sqlquery, deviceId, (err, deviceInDb) => {
+    if (err) {
+      res.status(500).send("Database query to find the device failed.");
+      return;
+    } else if (deviceInDb.length < 1) {
+      res.status(500).send("Device doesn't exist in the database.");
+      return;
+    }
+    res.render("devices/delete.html", {
+      title: "Device deleted",
+      id: deviceId,
+    });
+  });
+});
+
+// Delete device action page
+router.post("/:id", function (req, res) {
+  const deviceId = parseInt(req.params["id"]) || 0;
 
   // if deviceId is invalid
   if (deviceId < 1) {
     res.status(401).send("Device id is not correct.");
     return;
   }
-  let sqlquery = "DELETE FROM user_devices WHERE id = ?";
-  db.query(sqlquery, [deviceId], err => {
+  // check if the device being deleted actually exists in the database
+  let sqlquery = "SELECT * FROM user_devices WHERE id = ?";
+  db.query(sqlquery, deviceId, (err, deviceInDb) => {
     if (err) {
-      res.status(500).send("Database query to delete the device failed.");
+      res.status(500).send("Database query to find the device failed.");
+      return;
+    } else if (deviceInDb.length < 1) {
+      res.status(500).send("Device doesn't exist in the database.");
+      return;
     }
-    res.render("devices/deleted.html", {
-      title: "Device deleted",
-      id: deviceId,
+    sqlquery = "DELETE FROM user_devices WHERE id = ?";
+    db.query(sqlquery, deviceId, (err) => {
+      if (err) {
+        res.status(500).send("Database query to delete the device failed.");
+        return;
+      }
+      res.render("devices/deleted.html", {
+        title: "Device deleted",
+        id: deviceId,
+      });
     });
   });
 });
