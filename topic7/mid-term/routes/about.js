8f399eeb0935e75acd0f96a31b67cd93b4ec6acf
irfanullahjan <irfanullah.jan@outlook.com>
2021-07-10 07:41:01 +0500

    about page improved

--- topic7/mid-term/routes/about.js
+++ topic7/mid-term/routes/about.js
@@ -5,6 +5,14 @@ router.get("/", function (req, res) {
   // About page
   res.render("about.html", {
     title: "About",
+    appHost: process.env.APP_HOST,
+    appPort: process.env.APP_PORT,
+    db: {
+      "MySQL DB name": process.env.MYSQL_DATABASE,
+      "MySQL hostname": process.env.DB_HOST,
+      "MySQL port": process.env.DB_PORT,
+      "MySQL user": process.env.DB_USER,
+    },
   });
 });
 
